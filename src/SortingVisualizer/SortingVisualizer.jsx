ab4cb5e17ec2c7b55bbcb352c5ab676ee1ddc8dc
NicolasPCouts <nicolaspereiracoutinho@gmail.com>
2019-10-21 14:51:21 -0300

    implementing insertion sort animation part 2

--- src/SortingVisualizer/SortingVisualizer.jsx
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import './SortingVisualizer.css'
 import InsertionSort from '../SortingAlgorithms/InsertionSort'
-import { animationFrame } from 'C:/Users/nicol/AppData/Local/Microsoft/TypeScript/3.6/node_modules/rxjs/internal/scheduler/animationFrame';
+
+const NORMAL_COLOR = 'orange';
+const CHANGED_COLOR = 'green';
 
 export default class SortingVisualizer extends React.Component{
 
@@ -28,16 +30,36 @@ export default class SortingVisualizer extends React.Component{
     insertionSort(){
         let sortedArrayAnim = InsertionSort(this.state.arrayToSort);
         let arrayToSort = this.state.arrayToSort;
-        console.log(arrayToSort);
+        let arrayBars = document.getElementsByClassName('array-item');
+        let arrayBarsWithColorChanged = [];
+
+        //loop through all the animations
         for (let index = 0; index < sortedArrayAnim.length; index++) {
             const [i,j] = sortedArrayAnim[index];
+
             setTimeout(() => {
+                //set changed colors back to normal
+                if(index !== 0){
+                    arrayBarsWithColorChanged.forEach((element, index) => {
+                        arrayBars[element].style.backgroundColor = NORMAL_COLOR;
+                    });
+                    arrayBarsWithColorChanged = [];
+                }
+
                 let temp = arrayToSort[i];
+                //change array
                 arrayToSort[i] = arrayToSort[j];
                 arrayToSort[j] = temp;
+
+                //change div bar colors, unl
+                if(index != sortedArrayAnim.length - 1){
+                    arrayBars[i].style.backgroundColor = CHANGED_COLOR;
+                    arrayBars[j].style.backgroundColor = CHANGED_COLOR;
+                    arrayBarsWithColorChanged.push(i);
+                    arrayBarsWithColorChanged.push(j);
+                }
                 this.setState({ arrayToSort })
             }, 10);
-            
         }
     }
 
